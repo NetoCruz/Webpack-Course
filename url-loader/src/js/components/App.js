320bfcdd3278a09364856dd806be9fa6cf96efb6
unknown <galaxy.net0710@gmail.com>
2020-01-23 19:07:38 -0600

    Prepocesadores y formatos varios

---
+++ url-loader/src/js/components/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './Loader'
+import logo from '../../images/platzi.png'
+import video from '../../videos/que-es-core.mp4'
+console.log(data)
+
+function App(){
+    const [loaderList, setLoaderList]= useState([])
+    function handleClick(){
+        setLoaderList(data.loaders)
+    }
+    return(
+        <div>            
+            <p>que linda aplicacion en React.js</p> 
+              <img src={logo} alt="logo" width={40}/>
+             <video src={video} width={360} controls poster={logo}></video>
+            <ul>
+                {
+                   loaderList.map(item=><Loader data={item} key={item.id}/>)
+                }
+            </ul>
+            <button onClick={handleClick}>Mostra lo aprendido</button>
+        </div>
+    )
+}
+
+export default App
---
+++ url-loader/src/js/index.js
@@ -0,0 +1,7 @@
+import '../css/index.css'
+import '../css/index.css'
+import App from './components/App.js'
+import React from 'react'
+import {render} from 'react-dom'
+
+render(<App/>, document.getElementById('container'))
